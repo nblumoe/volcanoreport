e688bac58069e96f984192c1d9ee6e3881d9ebd5
Nils Blum-Oeste <nblumoeste@googlemail.com>
2011-06-10 00:04:03 +0200

    added date parser to xml parser
    - date does not get parsed yet
    
    Signed-off-by: Nils Blum-Oeste <nblumoeste@googlemail.com>

--- src/com/nbapps/volcanoreport/VolcanoXMLHandler.java
+++ src/com/nbapps/volcanoreport/VolcanoXMLHandler.java
@@ -1,16 +1,25 @@
 package com.nbapps.volcanoreport;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+
+import android.util.Log;
+
+
 
 public class VolcanoXMLHandler extends DefaultHandler {
 
 	protected String currentValue = "";
 	protected VolcanoInfo currentVolcanoInfo = null;
 	public static ArrayList<VolcanoInfo> volcanoList = null;
+	public static Date volcanoListDate = new Date(0);
 	
 	@Override
 	public void startDocument() {
@@ -41,6 +50,15 @@ public class VolcanoXMLHandler extends DefaultHandler {
 			if (splitTitle.length >= 3) {
 				currentVolcanoInfo
 						.setNewUnrest((splitTitle[2].contains("NEW")));
+			}
+		}
+		else if (localName.equalsIgnoreCase("pubDate")) {
+			SimpleDateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US);
+			try {
+				volcanoListDate = df.parse(currentValue);
+			} catch (ParseException e) {
+				Log.d("DEBUG", currentValue+"!");
+				Log.d("DEBUG", e.toString());
 			}
 		}
 		else if (localName.equalsIgnoreCase("description")) {
